b7f239b5a2f9d1a4b326b477922998dd4aa416e9
royalone <rorylm@westnet.com.au>
2020-04-27 14:58:25 +0800

    Reads sample conditions & performs test

--- script.py
+++ script.py
@@ -3,23 +3,24 @@ from sklearn.pipeline import Pipeline
 from sklearn.svm import SVC
 from sklearn.model_selection import cross_val_score
 from gensim.models.word2vec import Word2Vec
-from nltk.tokenize import TreebankWordTokenizer 
-  
-TRAIN_SET_PATH = "Reuters-r8-no-stop.txt"
+from csv import reader
 
+TRAIN_FILE = "SampleSetConditions.csv"
+TEST_FILE = "TestSet.csv"
+
+print("loading samples...",end="")
 X, y = [], []
-with open(TRAIN_SET_PATH, "r") as infile:
-    for line in infile:
-        label, text = line.split("\t")
-        # texts are already tokenized, just split on space
-        X.append(text.split())
-        y.append(label)
-X, y = np.array(X), np.array(y)
-#X=list of lists text, y = associated category
+with open(TRAIN_FILE,"r",encoding="utf-8") as infile:
+    csv_reader = reader(infile)
+    next(csv_reader,None) #ignore header
+    for row in csv_reader:
+        X.append(row[4].split())
+        y.append(row[1])
+print("...done")
 
 print ("total examples %s" % len(y))
 
-print("vectorising...")
+print("vectorising...",end="")
 model = Word2Vec(X, size=100, window=5, min_count=5, workers=2)
 w2v = {w: vec for w, vec in zip(model.wv.index2word, model.wv.vectors)}
 print("...done")
@@ -29,7 +30,6 @@ class MeanEmbeddingVectorizer(object):
     def __init__(self, word2vec):
         self.word2vec = word2vec
         if len(word2vec)>0:
-            #self.dim=len(word2vec[next(iter(glove_small))])
             self.dim=len(word2vec[next(iter(w2v))])
         else:
             self.dim=0
@@ -49,26 +49,34 @@ svm_w2v = Pipeline([
     ("word2vec vectorizer", MeanEmbeddingVectorizer(w2v)),
     ("SVM", SVC())])
 
-print("fitting...")
+print("fitting...",end="")
 svm_w2v.fit(X,y)
 print("...done")
 
-pre_test1 = "enex resources corp enex qtr loss shr not net loss profit revs year shr loss profit cts net loss profit revs reuter"
-pre_test2 = "douglas computer international year end shr cts net revs reuter "
-pre_test3 = "baker repeats hopes prime hike temporary treasury secretary james baker reiterated hope that this week rise prime rates temporary blip upwards hope that simply temporary blip upward past baker television interview cable news network interview airs tomorrow cnn released extracts remarks today baker repeated position that reaction financial markets tariffs japanese electronic goods showed importance united states not protectionist markets telling careful reuter "
 
+#======TESTING======
 
-tokenizer = TreebankWordTokenizer() 
-test1=tokenizer.tokenize(pre_test1) 
-test2=tokenizer.tokenize(pre_test2)
-test3=tokenizer.tokenize(pre_test3)
-
-tests = [test1, test2, test3]
-
-print (svm_w2v.predict(tests))
-
-
-'''
 unsorted_scores = (cross_val_score(svm_w2v, X, y, cv=5).mean())
-print(unsorted_scores)
-'''
+print("cross validation check accuracy:",round(unsorted_scores*100,2),"%\n")
+
+#read from testSet.csv
+tests, correct_cat = [], []
+with open(TEST_FILE,"r",encoding="utf-8") as infile:
+    csv_reader = reader(infile)
+    for row in csv_reader:
+        tests.append(row[4].split())
+        correct_cat.append(row[1])
+
+predictions = svm_w2v.predict(tests)
+
+correct = 0
+for x in range(len(predictions)):
+    if predictions[x] == correct_cat[x]:
+        print("\t-Test",x,"correctly categorised as",predictions[x])
+        correct+=1
+    else:
+        print("\t-Test",x,"FAILED. prediction:",predictions[x],"should be",correct_cat[x])
+
+print("correct percentage:",round((correct/len(predictions))*100,2),"%")
+
+
